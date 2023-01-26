b962a634a8d3b71e046d25732dd1f2a7e3bd8699
Rob Baily <rob-baily@users.noreply.github.com>
2023-01-26 10:16:06 -0500

    Common source (#5)
    * Add cmake include file for common source layout
    
    * README updates

---
+++ cmake/CommonSource.cmake
@@ -0,0 +1,28 @@
+# standard compile flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wall -Wno-unused-variable")
+set(CMAKE_CXX_STANDARD 17)
+
+# look for source files in src directory
+FILE(GLOB source_files "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
+# look for include files in include directory
+FILE(GLOB include_files "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h")
+
+# set up sources based on target name
+target_sources (${PROJECT_NAME}
+	PRIVATE
+	${source_files}
+)
+
+target_include_directories(${PROJECT_NAME}
+    PUBLIC
+        # where top-level project will look for the library's public headers
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        # where external projects will look for the library's public headers
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+)
+
+# without it public headers won't get installed
+set(
+    public_headers
+	${include_files}
+)
