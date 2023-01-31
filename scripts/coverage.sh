4ea1fd3f53dd38fa76637b54bdab7af9da082572
Rob Baily <rob.baily@leidos.com>
2023-01-31 18:15:41 -0500

    Set up debug symbols in Debian packages when appropriate Remove unused coverage script

--- scripts/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This builds the code coverage for a project based on files in the src directory
-
-set -ex
-
-# assumes a src folder under the current folder and a build folder to place the JSON in
-gcovr --json build/coverage.json -s --filter src
