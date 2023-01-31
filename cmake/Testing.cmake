10f52dcb86c71be2fb4c3b3efb5865fa822d1277
Rob Baily <rob-baily@users.noreply.github.com>
2023-01-31 12:28:42 -0500

    Add in CMake code coverage support (#7)
    * Add in CMake code coverage support
    * Automatically run tests during CMake build

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -4,12 +4,18 @@ IF(NOT DEFINED ENV{BUILD_ARCHITECTURE} AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/te
     find_package(GTest REQUIRED)
     # add add source files in the test directory
     file(GLOB_RECURSE TEST_SOURCES LIST_DIRECTORIES false test/*.h test/*.cpp)
-    add_executable(${PROJECT_NAME}_test 
+    set(UNIT_TEST ${PROJECT_NAME}_test)
+    add_executable(${UNIT_TEST}
         ${TEST_SOURCES}
     )
-    target_link_libraries(${PROJECT_NAME}_test PUBLIC 
+    target_link_libraries(${UNIT_TEST} PUBLIC 
         ${PROJECT_NAME}
         ${TEST_LINK_LIBRARIES}
         gtest)
-    add_test(NAME ${PROJECT_NAME}_test COMMAND ${PROJECT_NAME}_test)
+    add_test(NAME ${UNIT_TEST} COMMAND ${UNIT_TEST})
+    add_custom_target(
+        run_unit_test ALL
+        COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
+        DEPENDS ${UNIT_TEST}
+        )
 endif()
