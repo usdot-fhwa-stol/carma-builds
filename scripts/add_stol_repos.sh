fb8f158f38525d0d4afaa698a51bc68795e674a6
Rob Baily <rob.baily@leidos.com>
2023-03-09 10:48:03 -0500

    More udpates to set preferences

--- scripts/add_stol_repos.sh
+++ scripts/add_stol_repos.sh
@@ -1,29 +1,76 @@
 #!/bin/bash
 # Set up the STOL repositories to be used based on dev, feature and release branches
 
-# check for the branch name as as argument if not already set as environment varaible
-if [ -z "${BRANCH_NAME}" ]; then
-    BRANCH_NAME=$1
+set -e
+
+help()
+{
+    # Display Help
+    echo "Set up the STOL repositories for apt package downloads"
+    echo
+    echo "Syntax: $0 [-r|t]"
+    echo "options:"
+    echo "r     Use the argument as the GITHUB_REF_NAME"
+    echo "t     Indicates this is a tag"
+    echo "h     Print this Help."
+    echo
+}
+
+# Get the options
+while getopts "hr:t" option; do
+    case $option in
+        r) # branch reference name
+            GITHUB_REF_NAME=${OPTARG}
+            ;;
+        t) # GH tag
+            GITHUB_REF_TYPE=tag
+            ;;
+        h) # display Help
+            help
+            exit;;
+        *) # display Help
+            help
+            exit 1;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $# -gt 0 ]; then
+    help
+    exit 1
+fi
+
+# don't start echoing output until now
+#set -x
+
+# default to use the develop repo
+USE_DEVELOP=1
+
+# use dashes instead of underscores in our repo code names
+NO_UNDERSCORE_NAME=${GITHUB_REF_NAME/_/-}
+DEVELOP_BRANCHES="carma-develop develop"
+
+# check for feature branches
+if [[ ${GITHUB_REF_NAME} =~ feature/.* ]]; then
+    APT_CODENAME=feature-${NO_UNDERSCORE_NAME##*/}
+
+# check for release candidate branches, do not use develop for these
+elif [[ ${GITHUB_REF_NAME} =~ release/.* ]]; then
+    APT_CODENAME=candidate-${NO_UNDERSCORE_NAME##*/}
+    USE_DEVELOP=0
+
+# check for release tags, do not use develop for these
+elif [[ $GITHUB_REF_TYPE = tag ]]; then
+    APT_CODENAME=release-${NO_UNDERSCORE_NAME##*/}
+    USE_DEVELOP=0
+
+# default to pull from for all repos
+else
+    APT_CODENAME=develop
 fi
 
 STOL_APT_AWS_BUCKET_NAME=stol-apt-repository
 STOL_APT_REPOSITORY=http://s3.amazonaws.com/${STOL_APT_AWS_BUCKET_NAME}
-# default to use the develop repo
-USE_DEVELOP=1
-
-if [ ! -z "${BRANCH_NAME}" ]; then
-    if [[ ${BRANCH_NAME} =~ feature/.* ]]; then
-        APT_CODENAME=feature-$(echo ${BRANCH_NAME} | sed -E -e 's/feature\/(.*)/\1/g')
-    fi
-    if [[ ${BRANCH_NAME} =~ candidate/.* ]]; then
-        APT_CODENAME=candidate-$(echo ${BRANCH_NAME} | sed -E -e 's/candidate\/(.*)/\1/g')
-        USE_DEVELOP=0
-    fi
-    if [[ ${BRANCH_NAME} =~ release/.* ]]; then
-        APT_CODENAME=release-$(echo ${BRANCH_NAME} | sed -E -e 's/release\/(.*)/\1/g')
-        USE_DEVELOP=0
-    fi
-fi
 
 LIST_FILE=/etc/apt/sources.list.d/carma.list
 PREFERENCE_FILE=/etc/apt/preferences.d/carma.pref
@@ -32,18 +79,18 @@ rm -f $PREFERENCE_FILE
 if [ $USE_DEVELOP -eq 1 ]; then
     echo "deb [trusted=yes] ${STOL_APT_REPOSITORY} develop main" >> $LIST_FILE
 fi
-if [ ! -z "$APT_CODENAME" ]; then
+if [[ ! -z "$APT_CODENAME" && "$APT_CODENAME" != "develop" ]]; then
     echo "deb [trusted=yes] ${STOL_APT_REPOSITORY} ${APT_CODENAME} main" >> $LIST_FILE
     # pin this repository
 cat << EOF >> $PREFERENCE_FILE
     Package: *
-    Pin: ${APT_CODENAME} o=${STOL_APT_AWS_BUCKET_NAME
+    Pin: ${APT_CODENAME} o=${STOL_APT_AWS_BUCKET_NAME}
     Pin-Priority: 999
 EOF
 
 fi
 
-echo "BRANCH_NAME=$BRANCH_NAME APT_CODENAME=$APT_CODENAME USE_DEVELOP=$USE_DEVELOP"
+echo "GITHUB_REF_NAME=$GITHUB_REF_NAME APT_CODENAME=$APT_CODENAME USE_DEVELOP=$USE_DEVELOP"
 echo "<----- List file below ----->"
 cat $LIST_FILE
 if [ -f $PREFERENCE_FILE ]; then
