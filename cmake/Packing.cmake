349db5dcd768fd68b1a503e06a3e8cb77a57b595
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-02-25 15:28:44 -0500

    Update Packing.cmake

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -81,7 +81,6 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 # package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
 # you'll get some-application_0.9.2_amd64.deb (note the underscores too)
 # set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-set(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}-${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}-${UBUNTU_CODENAME}")
 # that is if you want every group to have its own package,
 # although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
 # and CPACK_DEB_COMPONENT_INSTALL is set to YES
@@ -99,5 +98,6 @@ if (DEFINED ENV{BUILD_ARCHITECTURE})
     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE $ENV{BUILD_ARCHITECTURE})
     set(CPACK_OBJCOPY_EXECUTABLE ${CMAKE_OBJCOPY})
 endif()
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}-${UBUNTU_CODENAME}")
 
 include(CPack)
