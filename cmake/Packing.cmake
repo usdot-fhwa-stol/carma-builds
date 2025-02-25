f158f1ecebb1f01e2666903611922306d02133f4
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-02-25 17:09:57 -0500

    Update Packing.cmake

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -63,7 +63,7 @@ set(CPACK_RESOURCE_FILE_LICENSE "$ENV{CARMA_OPT_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
 # package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
-# you'll get some-application_0.9.2_amd64.deb (note the underscores too)
+# you'll get some-application_0.9.2_jammy_amd64.deb (note the underscores too)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 # that is if you want every group to have its own package,
 # although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
